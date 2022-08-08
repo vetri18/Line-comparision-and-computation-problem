6cda0160b6bf47ef836993220cb20b0b51ee74dc
vetrivel <vetrivel.kce@gmail.com>
2022-08-08 16:22:31 +0530

    uc two

--- Program.cs
+++ Program.cs
@@ -1,14 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
-namespace practiceproblem1
+namespace practiceproblem2
 {
     class Line
     {
         public static void Main(string[] args)
         {
-            lineucone.LineModel();
-         
+            Lineuctwo.LineModel();
         }
-}
+    }
 }
